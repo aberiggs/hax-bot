0ec22b58004ca59c1b88d92ba530da4bcd91e2c2
Abe Riggs <sprocket710@gmail.com>
2018-06-17 20:09:49 -0700

    The Birth of Hax-Bot...

---
+++ Hax-Bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+Client = discord.Client() #Initialise Client 
+client = commands.Bot(command_prefix = "~") #Initialise client bot
+
+@client.event 
+async def on_ready():
+    print("Hax-Bot Online") #This will be called when the bot connects to the server
+
+@client.event
+async def on_message(message):
+    if message.content == "~clear":
+        await client.purge_from(message.channel, limit=none, check=100, before=None, after=None, around=None)
+
+async def my_background_task():
+    await client.wait_until_ready()
+    counter = 0
+    channel = discord.Object(id= '318857865609871360')
+    while not client.is_closed:
+        counter += 1
+        await client.purge_from(client.get_channel('318857865609871360'), limit=1000, check=None, before=None, after=None, around=None)
+        await asyncio.sleep(86400) # task runs every 24 hours
+      
+client.loop.create_task(my_background_task())
+client.run("NDU3Nzg0NzM3MjY4NzYwNTk4.DgeMnQ.un774saWMTJmvcwjb2mIfTfUPLs") #Replace token with your bots token
